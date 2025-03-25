c016f8d8189df4e5fad3a5d5712606a6e5ca2634
Adiel <adielssaad@gmail.com>
2025-03-25 18:04:11 +0000

    test(fundme): enhance FundMe test suite with comprehensive test cases

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,9 +7,15 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+
+    address USER = makeAddr("user"); // make a new address
+    uint256 constant SEND_VALUE = 0.1 ether; // constant value to send
+    uint256 constant STARTING_BALANCE = 10 ether; // constant starting balance
+
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
     }
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
@@ -23,4 +29,22 @@ contract FundMeTest is Test {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
     }
+
+    function testFundFailsWithoutEnoughtEth() public {
+        vm.expectRevert(); // expect the fund function to revert
+        fundMe.fund(); // call the fund function
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // The next TX will be made by the USER
+        fundMe.fund{value: SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.expectRevert(); // expect the withdraw function to revert
+        fundMe.withdraw(); // call the withdraw function
+    }
+    
 }
